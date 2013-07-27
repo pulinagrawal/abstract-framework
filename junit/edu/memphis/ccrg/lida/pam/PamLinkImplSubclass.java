cbcca27e043f116b8dee80f14d62fbb0e20f7504
skugele <skugele@55eb51e6-de06-0410-832d-d63f1ec65357>
2013-07-27 14:40:46 +0000

    Removed junit tests from abstract framework.  Most were implementation specific.  We need new tests.

--- junit/edu/memphis/ccrg/lida/pam/PamLinkImplSubclass.java
+++
@@ -1,7 +0,0 @@
-package edu.memphis.ccrg.lida.pam;
-
-import edu.memphis.ccrg.lida.pam.ns.PamLinkImpl;
-
-public class PamLinkImplSubclass extends PamLinkImpl {
-
-}
--- junit/edu/memphis/ccrg/lida/pam/PamNodeImplSubclass.java
+++
@@ -1,7 +0,0 @@
-package edu.memphis.ccrg.lida.pam;
-
-import edu.memphis.ccrg.lida.pam.ns.PamNodeImpl;
-
-public class PamNodeImplSubclass extends PamNodeImpl {
-
-}
