03b74c97d4a215ccd8a934cd6325b91fccccb70b
skugele <skugele@55eb51e6-de06-0410-832d-d63f1ec65357>
2013-07-27 16:44:17 +0000

    Added ReceiveMode enum with TOP_DOWN and BOTTOM_UP values (used with receive method). Added javadoc.

--- src/edu/memphis/ccrg/lida/pam/PerceptualAssociativeMemory.java
+++ src/edu/memphis/ccrg/lida/pam/PerceptualAssociativeMemory.java
@@ -11,21 +11,57 @@ import edu.memphis.ccrg.lida.framework.FrameworkModule;
 import edu.memphis.ccrg.lida.framework.shared.CognitiveContentStructure;
 
 /**
+ * 
+ * Interface for the PerceptualAssociativeMemory module. The interface supports
+ * top-down and bottom-up input modes, the registering of listeners (observer
+ * pattern), and learning via the learn method.
+ * 
  * @author Sean Kugele
- *
+ * 
  */
 public interface PerceptualAssociativeMemory extends FrameworkModule {
-    
+
+    /**
+     * An enum that specifies the supported "mode" parameters for the receive
+     * method.
+     */
+    public enum ReceiveMode {
+
+        /**
+         * Top-Down
+         */
+        TOP_DOWN,
+
+        /**
+         * Bottom-Up
+         */
+        BOTTOM_UP
+    }
+
     /**
      * Adds {@link PamListener}.
      * 
      * @param pl
-     *            listener
+     *            the listener
      */
     public void addPamListener(PamListener pl);
-    
-    public void receive(Enum mode, CognitiveContentStructure content);
-    
+
+    /**
+     * Receives {@link CognitiveContentStructure} in the specified mode.
+     * 
+     * @param mode
+     *            a supported {@link ReceiveMode} value
+     * @param content
+     *            the cognitive content to be received
+     */
+    public void receive(ReceiveMode mode, CognitiveContentStructure content);
+
+    /**
+     * Update associations in PAM based on the supplied
+     * {@link CognitiveContentStructure}.
+     * 
+     * @param content
+     *            the cognitive content to be learned
+     */
     public void learn(CognitiveContentStructure content);
 }
-
