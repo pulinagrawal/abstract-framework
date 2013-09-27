2e3be2c9a847c0d9c173fdb107af0cec0887e051
skugele <skugele@55eb51e6-de06-0410-832d-d63f1ec65357>
2013-09-27 01:43:29 +0000

    Removed unneeded imports in XmlUtils

--- src/edu/memphis/ccrg/lida/framework/initialization/XmlUtils.java
+++ src/edu/memphis/ccrg/lida/framework/initialization/XmlUtils.java
@@ -10,13 +10,11 @@
  */
 package edu.memphis.ccrg.lida.framework.initialization;
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
