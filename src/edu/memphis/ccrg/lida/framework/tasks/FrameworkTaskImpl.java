b44938614f96633f3ded2a521cb9378cb1effb71
skugele <skugele@55eb51e6-de06-0410-832d-d63f1ec65357>
2015-02-28 15:09:17 +0000

    The ModuleUsage parameter was removed from FrameworkTaskImpl.  This was causing complications in the xml configuration file, and did not seem to have much functional purpose.  Also added initAssociatedModules.

--- src/edu/memphis/ccrg/lida/framework/tasks/FrameworkTaskImpl.java
+++ src/edu/memphis/ccrg/lida/framework/tasks/FrameworkTaskImpl.java
@@ -10,10 +10,13 @@
  */
 package edu.memphis.ccrg.lida.framework.tasks;
 
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import edu.memphis.ccrg.lida.framework.FrameworkModule;
+import edu.memphis.ccrg.lida.framework.ModuleName;
+import edu.memphis.ccrg.lida.framework.initialization.GlobalInitializer;
 import edu.memphis.ccrg.lida.framework.shared.activation.LearnableImpl;
 
 /**
@@ -23,214 +26,238 @@ import edu.memphis.ccrg.lida.framework.shared.activation.LearnableImpl;
  * @author Javier Snaider
  * @author Ryan J. McCall
  */
-public abstract class FrameworkTaskImpl extends LearnableImpl implements
-		FrameworkTask {
+public abstract class FrameworkTaskImpl extends LearnableImpl implements FrameworkTask {
 
-	private static final Logger logger = Logger
-			.getLogger(FrameworkTaskImpl.class.getCanonicalName());
+    private static final Logger logger = Logger.getLogger(FrameworkTaskImpl.class
+            .getCanonicalName());
 
-	private final static int defaultTicksPerRun = 1;
-	private static long nextTaskID;
+    private final static int defaultTicksPerRun = 1;
+    private static long nextTaskID;
 
-	/*
-	 * frequency in ticks
-	 */
-	private int ticksPerRun = defaultTicksPerRun;
-	private long taskID;
-	private long nextExcecutionTicksPerRun = defaultTicksPerRun;
-	/**
-	 * {@link TaskStatus} of this task. Initial value is
-	 * {@link TaskStatus#RUNNING}.
-	 */
-	protected TaskStatus status = TaskStatus.RUNNING;
-	private TaskSpawner controllingTS;
-	private long scheduledTick;
-	private final String taskName;
+    /*
+     * frequency in ticks
+     */
+    private int ticksPerRun = defaultTicksPerRun;
+    private long taskID;
+    private long nextExcecutionTicksPerRun = defaultTicksPerRun;
+    /**
+     * {@link TaskStatus} of this task. Initial value is
+     * {@link TaskStatus#RUNNING}.
+     */
+    protected TaskStatus status = TaskStatus.RUNNING;
+    private TaskSpawner controllingTS;
+    private long scheduledTick;
+    private final String taskName;
 
-	/**
-	 * Constructs a {@link FrameworkTaskImpl} with default ticksPerRun
-	 */
-	public FrameworkTaskImpl() {
-		this(defaultTicksPerRun, null);
-	}
+    /**
+     * Constructs a {@link FrameworkTaskImpl} with default ticksPerRun
+     */
+    public FrameworkTaskImpl() {
+        this(defaultTicksPerRun, null);
+    }
 
-	/**
-	 * Constructs a {@link FrameworkTaskImpl} with specified ticksPerRun
-	 * 
-	 * @param ticksPerRun
-	 *            task's run frequency
-	 */
-	public FrameworkTaskImpl(int ticksPerRun) {
-		this(ticksPerRun, null);
-	}
+    /**
+     * Constructs a {@link FrameworkTaskImpl} with specified ticksPerRun
+     * 
+     * @param ticksPerRun
+     *            task's run frequency
+     */
+    public FrameworkTaskImpl(int ticksPerRun) {
+        this(ticksPerRun, null);
+    }
 
-	/**
-	 * Constructs a {@link FrameworkTaskImpl} with default ticksPerRun and
-	 * specified controlling {@link TaskSpawner}
-	 * 
-	 * @param ticksPerRun
-	 *            task's run frequency
-	 * @param ts
-	 *            controlling {@link TaskSpawner}
-	 */
-	public FrameworkTaskImpl(int ticksPerRun, TaskSpawner ts) {
-		taskID = nextTaskID++;
-		controllingTS = ts;
-		setTicksPerRun(ticksPerRun);
-		taskName = getClass().getSimpleName() + "[" + taskID + "]";
-	}
+    /**
+     * Constructs a {@link FrameworkTaskImpl} with default ticksPerRun and
+     * specified controlling {@link TaskSpawner}
+     * 
+     * @param ticksPerRun
+     *            task's run frequency
+     * @param ts
+     *            controlling {@link TaskSpawner}
+     */
+    public FrameworkTaskImpl(int ticksPerRun, TaskSpawner ts) {
+        taskID = nextTaskID++;
+        controllingTS = ts;
+        setTicksPerRun(ticksPerRun);
+        taskName = getClass().getSimpleName() + "[" + taskID + "]";
+    }
 
-	@Override
-	public long getScheduledTick() {
-		return scheduledTick;
-	}
+    @Override
+    public long getScheduledTick() {
+        return scheduledTick;
+    }
 
-	@Override
-	public void setScheduledTick(long t) {
-		scheduledTick = t;
-	}
+    @Override
+    public void setScheduledTick(long t) {
+        scheduledTick = t;
+    }
 
-	/**
-	 * This method should not be called directly nor should it be overridden.
-	 * Override {@link #runThisFrameworkTask()} instead.
-	 * 
-	 * @see java.util.concurrent.Callable#call()
-	 */
-	@Override
-	public FrameworkTask call() {
-		nextExcecutionTicksPerRun = ticksPerRun;
-		try {
-			runThisFrameworkTask();
-		} catch (Exception e) {
-			logger
-					.log(
-							Level.WARNING,
-							"Exception encountered during the execution of task {1}. \n {2}",
-							new Object[] { TaskManager.getCurrentTick(), this,
-									e });
-			e.printStackTrace();
-		}
-		if (controllingTS != null) {
-			try {
-				controllingTS.receiveFinishedTask(this);
-			} catch (Exception e) {
-				logger
-						.log(
-								Level.WARNING,
-								"Exception encountered during the execution of method 'receiveFinishedTask' in TaskSpawner: {1} \n {2}",
-								new Object[] { TaskManager.getCurrentTick(),
-										this, e });
-				e.printStackTrace();
-			}
-		} else {
-			logger.log(Level.WARNING,
-					"Task {1} does not have an assigned TaskSpawner",
-					new Object[] { TaskManager.getCurrentTick(), this });
-		}
-		return this;
-	}
+    /**
+     * This method should not be called directly nor should it be overridden.
+     * Override {@link #runThisFrameworkTask()} instead.
+     * 
+     * @see java.util.concurrent.Callable#call()
+     */
+    @Override
+    public FrameworkTask call() {
+        nextExcecutionTicksPerRun = ticksPerRun;
+        try {
+            runThisFrameworkTask();
+        } catch (Exception e) {
+            logger.log(Level.WARNING,
+                    "Exception encountered during the execution of task {1}. \n {2}",
+                    new Object[] { TaskManager.getCurrentTick(), this, e });
+            e.printStackTrace();
+        }
+        if (controllingTS != null) {
+            try {
+                controllingTS.receiveFinishedTask(this);
+            } catch (Exception e) {
+                logger.log(
+                        Level.WARNING,
+                        "Exception encountered during the execution of method 'receiveFinishedTask' in TaskSpawner: {1} \n {2}",
+                        new Object[] { TaskManager.getCurrentTick(), this, e });
+                e.printStackTrace();
+            }
+        } else {
+            logger.log(Level.WARNING, "Task {1} does not have an assigned TaskSpawner",
+                    new Object[] { TaskManager.getCurrentTick(), this });
+        }
+        return this;
+    }
 
-	/**
-	 * To be overridden by extending classes. Overriding method should execute a
-	 * handful of statements considered to constitute a single iteration of the
-	 * task. For example, a codelet might look in a buffer for some content and
-	 * make a change to it in a single iteration. The overriding method may also
-	 * change the {@link TaskStatus} of a task. For example, if the task should
-	 * only run once and stop, then the method {@link #cancel()} may be used to
-	 * stop the task from further execution (calls of this
-	 * {@link #runThisFrameworkTask()} beyond the current one.
-	 */
-	protected abstract void runThisFrameworkTask();
+    /**
+     * To be overridden by extending classes. Overriding method should execute a
+     * handful of statements considered to constitute a single iteration of the
+     * task. For example, a codelet might look in a buffer for some content and
+     * make a change to it in a single iteration. The overriding method may also
+     * change the {@link TaskStatus} of a task. For example, if the task should
+     * only run once and stop, then the method {@link #cancel()} may be used to
+     * stop the task from further execution (calls of this
+     * {@link #runThisFrameworkTask()} beyond the current one.
+     */
+    protected abstract void runThisFrameworkTask();
 
-	@Override
-	public synchronized void setTaskStatus(TaskStatus s) {
-		if (status == TaskStatus.CANCELED) {
-			logger
-					.log(
-							Level.WARNING,
-							"Cannot set TaskStatus to {1}. TaskStatus is already CANCELED so it cannot be modified again.",
-							new Object[] { TaskManager.getCurrentTick(), s });
-		} else {
-			status = s;
-		}
-	}
+    @Override
+    public void init() {
+        super.init();
+    }
 
-	@Override
-	public TaskStatus getTaskStatus() {
-		return status;
-	}
+    /**
+     * Associated modules with this {@code FrameworkTask}. The modules that will
+     * be associated are based on the "associatedModules" parameter value for
+     * this {@code FrameworkTask}, which is set in the factory configuration
+     * file.
+     * 
+     * @param moduleMap
+     *            a mapping from module names to module object references
+     */
+    @Override
+    public void initAssociatedModules(Map<ModuleName, FrameworkModule> moduleMap) {
+        if (moduleMap == null) {
+            return;
+        }
+        
+        String assocModulesParam = getParam("associatedModules", "");
+        if (!(assocModulesParam == null || assocModulesParam.isEmpty())) {
+            String[] assocModules = assocModulesParam.split(",");
+            for (String m : assocModules) {
+                ModuleName name = ModuleName.getModuleName(m);
+                FrameworkModule module = moduleMap.get(name);
 
-	@Override
-	public long getTaskId() {
-		return taskID;
-	}
+                setAssociatedModule(module);
+            }
+        }
+    }
 
-	@Override
-	public synchronized int getTicksPerRun() {
-		return ticksPerRun;
-	}
+    @Override
+    public synchronized void setTaskStatus(TaskStatus s) {
+        if (status == TaskStatus.CANCELED) {
+            logger.log(
+                    Level.WARNING,
+                    "Cannot set TaskStatus to {1}. TaskStatus is already CANCELED so it cannot be modified again.",
+                    new Object[] { TaskManager.getCurrentTick(), s });
+        } else {
+            status = s;
+        }
+    }
 
-	@Override
-	public synchronized void setTicksPerRun(int ticks) {
-		if (ticks > 0) {
-			ticksPerRun = ticks;
-			setNextTicksPerRun(ticks);
-		}
-	}
+    @Override
+    public TaskStatus getTaskStatus() {
+        return status;
+    }
 
-	@Deprecated
-	@Override
-	public void stopRunning() {
-		cancel();
-	}
+    @Override
+    public long getTaskId() {
+        return taskID;
+    }
 
-	@Override
-	public void cancel() {
-		setTaskStatus(TaskStatus.CANCELED);
-	}
+    @Override
+    public synchronized int getTicksPerRun() {
+        return ticksPerRun;
+    }
 
-	@Override
-	public TaskSpawner getControllingTaskSpawner() {
-		return controllingTS;
-	}
+    @Override
+    public synchronized void setTicksPerRun(int ticks) {
+        if (ticks > 0) {
+            ticksPerRun = ticks;
+            setNextTicksPerRun(ticks);
+        }
+    }
 
-	@Override
-	public void setControllingTaskSpawner(TaskSpawner ts) {
-		controllingTS = ts;
-	}
+    @Deprecated
+    @Override
+    public void stopRunning() {
+        cancel();
+    }
 
-	@Override
-	public long getNextTicksPerRun() {
-		return nextExcecutionTicksPerRun;
-	}
+    @Override
+    public void cancel() {
+        setTaskStatus(TaskStatus.CANCELED);
+    }
 
-	@Override
-	public void setNextTicksPerRun(long tick) {
-		nextExcecutionTicksPerRun = tick;
-	}
+    @Override
+    public TaskSpawner getControllingTaskSpawner() {
+        return controllingTS;
+    }
 
-	/**
-	 * Subclasses may override this method.
-	 */
-	@Override
-	public void setAssociatedModule(FrameworkModule module, String moduleUsage) {
-	}
+    @Override
+    public void setControllingTaskSpawner(TaskSpawner ts) {
+        controllingTS = ts;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof FrameworkTaskImpl) {
-			return taskID == ((FrameworkTaskImpl) o).getTaskId();
-		}
-		return false;
-	}
+    @Override
+    public long getNextTicksPerRun() {
+        return nextExcecutionTicksPerRun;
+    }
 
-	@Override
-	public int hashCode() {
-		return (int) taskID;
-	}
+    @Override
+    public void setNextTicksPerRun(long tick) {
+        nextExcecutionTicksPerRun = tick;
+    }
 
-	@Override
-	public String toString() {
-		return taskName;
-	}
+    /**
+     * Subclasses may override this method.
+     */
+    @Override
+    public void setAssociatedModule(FrameworkModule module) {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof FrameworkTaskImpl) {
+            return taskID == ((FrameworkTaskImpl) o).getTaskId();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) taskID;
+    }
+
+    @Override
+    public String toString() {
+        return taskName;
+    }
 }
